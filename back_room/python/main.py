20be62f880107a548b3328f5e5c648a27702ed4f
Marco Cesar da Silva <marcocesardasilva@gmail.com>
2023-06-13 10:03:37 -0300

    Atualização

--- back_room/python/main.py
+++ back_room/python/main.py
@@ -1,6 +1,6 @@
 # importar de outro arquivo
 from extract import *
-# from caged_transform import *
+from transform import *
 from load import *
 
 ##########################################################################
@@ -21,33 +21,25 @@ dataset_fonte = dataset_exist(client,dataset_name)
 table_movimentacao, table_localidade, table_trabalhador, table_periodo, table_empregador, table_fato_caged = table_exist(client,dataset_fonte)
 
 ##########################################################################
-#                            Executar extrações                          #
+#                               Executar ETL                             #
 ##########################################################################
-# Verificar próximo ano e mês a baixar dados
-proximo_ano, proximo_mes = update_date(client,credentials,dataset_fonte,table_periodo)
-# Baixar arquivos
-download_files(proximo_ano,proximo_mes,data_folder)
+while True:
+    # Verificar próximo ano e mês a baixar dados
+    proximo_ano, proximo_mes = update_date(client,credentials,dataset_fonte,table_periodo)
+    # Baixar arquivos
+    download_files(proximo_ano,proximo_mes,data_folder)
+    # Ler todos os arquivos, tratar e agrupar em um único dataframe
+    df_group = group_files(data_folder)
+    # Criar dfs da fato e das dimensões
+    df_movimentacao, df_localidade, df_trabalhador, df_periodo, df_empregador, df_fato_caged = create_dfs(df_group)
+    # Incluir tabelas e dfs em uma biblioteca
+    tables_dfs = {
+        table_movimentacao:df_movimentacao,
+        table_localidade:df_localidade,
+        table_trabalhador:df_trabalhador,
+        table_periodo:df_periodo,
+        table_empregador:df_empregador,
+        table_fato_caged:df_fato_caged}
+    # Carregar os dados no GCP
+    load_data(tables_dfs,client,dataset_fonte)
 
-'''
-##########################################################################
-#                         Executar transformações                        #
-##########################################################################
-# Ler todos os arquivos, tratar e agrupar em um único dataframe
-df_group = group_files(data_folder)
-# Criar dfs da fato e das dimensões
-df_movimentacao, df_localidade, df_trabalhador, df_periodo, df_empregador, df_fato_caged = create_dfs(df_group)
-
-##########################################################################
-#                              Executar Carga                            #
-##########################################################################
-# Incluir tabelas e dfs em uma biblioteca
-tables_dfs = {
-    table_movimentacao:df_movimentacao,
-    table_localidade:df_localidade,
-    table_trabalhador:df_trabalhador,
-    table_periodo:df_periodo,
-    table_empregador:df_empregador,
-    table_fato_caged:df_fato_caged}
-# Carregar os dados no GCP
-load_data(tables_dfs,client,dataset_fonte)
-'''
